b9f3d56c8523027311ffecc30f4e1ba1cac3474d
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-04-19 16:49:16 +0300

    massive update

---
+++ 2roma/JS/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+
+    var money = JSON.parse(sessionStorage.getItem('counter'));
+    $('.counter').html(money);
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 500
+    });
+
+
+    
+    
+    
+
+
+});
