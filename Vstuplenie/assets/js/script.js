6514d41fe6d888c2248cf213c18e936638054e48
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-01 15:37:52 +0300

    vstuplenie_adaptivka

--- Vstuplenie/assets/js/script.js
+++ Vstuplenie/assets/js/script.js
@@ -1,13 +1,17 @@
-function instr() {
-  var btn = document.getElementById("btn");
-  var page = document.getElementById("page");
-  var instruction = document.getElementById("instruction");
-  if (instruction.style.display === "block") {
-    instruction.style.display = "none";
-    page.style.filter = "blur()";
-  } else {
-    instruction.style.display = "block";
-    page.style.filter = "blur(10px)";
-  }
-}
+$(function(){
+
+
+  $('#btn').on('click', function(){
+    $('#instruction').fadeIn();
+    $('#page').addClass('blur');
+
+  });
+
+
+  $('#cross').on('click', function(){
+    $('#instruction').fadeOut();
+    $('#page').removeClass('blur');
+  });
+
+});
 
