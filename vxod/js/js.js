3db54823812a3141a4f05ccf7e0f1a2dd0e2a743
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-07 14:26:44 +0300

    zz

--- vxod/js/js.js
+++ vxod/js/js.js
@@ -25,36 +25,4 @@ $('.prev').on('click', function(){
     }
 });
 
-jQuery(document).ready(function($){
 
-    var money = JSON.parse(sessionStorage.getItem('counter'));
-    $('.counter').html(money);
-
-    $('.counter').counterUp({
-        delay: 10,
-        time: 500
-    });
-
-
-    if(money<=0){
-        $(function(){
-          var height = $(document).height();
-          $('#wdw').innerHeight(height);
-          $('.page').css("filter", "blur(10px)");
-          $('#wdw').fadeIn();
-        });
-      };
-    
-    $('#roma1').on('click', function(){
-
-        money=money-14;
-        sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-        location.href = 'https://jazkydev.github.io/activePassive';
-
-        
-        });
-    
-
-
-
-});
---
+++ vxod/js/script.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($){
+
+    var money = JSON.parse(sessionStorage.getItem('counter'));
+    $('.counter').html(money);
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 500
+    });
+
+
+    if(money<=0){
+        $(function(){
+          var height = $(document).height();
+          $('#wdw').innerHeight(height);
+          $('.page').css("filter", "blur(10px)");
+          $('#wdw').fadeIn();
+        });
+      };
+    
+    $('#btn').on('click', function(){
+
+        money=money-4;
+        sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    });
+    
+
+
+
+});
