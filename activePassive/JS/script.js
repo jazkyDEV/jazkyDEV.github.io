c5338ea5a95e91cd51b802fcc0f992bffa22f517
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-08 00:53:38 +0300

    ggs

--- activePassive/JS/script.js
+++ activePassive/JS/script.js
@@ -1,55 +1,43 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+  var money = JSON.parse(sessionStorage.getItem("counter"));
+  $(".counter").html(money);
 
-    var money = JSON.parse(sessionStorage.getItem('counter'));
-    $('.counter').html(money);
+  $(".counter").counterUp({
+    delay: 10,
+    time: 500,
+  });
 
-    $('.counter').counterUp({
-        delay: 10,
-        time: 500
+  if (money <= 0) {
+    $(function () {
+      var height = $(document).height();
+      $("#wdw").innerHeight(height);
+      $(".page").css("filter", "blur(10px)");
+      $("#wdw").fadeIn();
     });
+  }
 
-    if(money<=0){
-        $(function(){
-          var height = $(document).height();
-          $('#wdw').innerHeight(height);
-          $('.page').css("filter", "blur(10px)");
-          $('#wdw').fadeIn();
-        });
-      };
+  $(".kotok").on("click", function () {
+    money = money - 3;
+    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
+  });
 
-      $('#kotok').on('click', function(){
+  $(".lot").on("click", function () {
+    money = money - 87;
+    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
+  });
 
-        money=money-3;
-        sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-        });
+  $(".lebyajiy").on("click", function () {
+    money = money - 21;
+    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
+  });
 
+  $(".spaPlanet").on("click", function () {
+    money = money - 27;
+    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
+  });
 
-        $('#lot').on('click', function(){
-
-          money=money-87;
-          sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-          });
-
-        $('#leb').on('click', function(){
-
-            money=money-21;
-            sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-            });
-
-
-        $('#spaPlanet').on('click', function(){
-
-              money=money-27;
-              sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-              });
-
-
-        $('#roup').on('click', function(){
-
-              money=money-6;
-              sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
-              });
-    
-
-
+  $("#roup").on("click", function () {
+    money = money - 6;
+    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
+  });
 });
