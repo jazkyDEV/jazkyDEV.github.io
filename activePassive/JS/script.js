d67bcef59024ede689e2d590172919f2818728a1
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-08 15:16:23 +0300

    gg

--- activePassive/JS/script.js
+++ activePassive/JS/script.js
@@ -1,43 +1,55 @@
-jQuery(document).ready(function ($) {
-  var money = JSON.parse(sessionStorage.getItem("counter"));
-  $(".counter").html(money);
+jQuery(document).ready(function($){
 
-  $(".counter").counterUp({
-    delay: 10,
-    time: 500,
-  });
+    var money = JSON.parse(sessionStorage.getItem('counter'));
+    $('.counter').html(money);
 
-  if (money <= 0) {
-    $(function () {
-      var height = $(document).height();
-      $("#wdw").innerHeight(height);
-      $(".page").css("filter", "blur(10px)");
-      $("#wdw").fadeIn();
+    $('.counter').counterUp({
+        delay: 10,
+        time: 500
     });
-  }
 
-  $(".kotok").on("click", function () {
-    money = money - 3;
-    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
-  });
+    if(money<=0){
+        $(function(){
+          var height = $(document).height();
+          $('#wdw').innerHeight(height);
+          $('.page').css("filter", "blur(10px)");
+          $('#wdw').fadeIn();
+        });
+      };
 
-  $(".lot").on("click", function () {
-    money = money - 87;
-    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
-  });
+      $('#kotok').on('click', function(){
 
-  $(".lebyajiy").on("click", function () {
-    money = money - 21;
-    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
-  });
+        money=money-3;
+        sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+        });
 
-  $(".spaPlanet").on("click", function () {
-    money = money - 27;
-    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
-  });
 
-  $("#roup").on("click", function () {
-    money = money - 6;
-    sessionStorage.setItem("counter", JSON.stringify(money.toFixed(1)));
-  });
+        $('#lot').on('click', function(){
+
+          money=money-87;
+          sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+          });
+
+        $('#leb').on('click', function(){
+
+            money=money-21;
+            sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+            });
+
+
+        $('#spaPlanet').on('click', function(){
+
+              money=money-27;
+              sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+              });
+
+
+        $('#roup').on('click', function(){
+
+              money=money-6;
+              sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+              });
+    
+
+
 });
