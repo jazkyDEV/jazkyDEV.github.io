9c6710a77dd9abdc91641903722a444d457bcb4b
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-25 19:06:14 +0300

    vxod

--- vxod/js/script.js
+++ vxod/js/script.js
@@ -37,12 +37,4 @@ jQuery(document).ready(function($){
         money=money-4;
         sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
     });
-    
-
-
-      /* while(true){
-        $('.flipper').css({'transform' : 'scale(1.03)', 'transform' : 'rotate(-15deg)'}).delay(100);
-        $('.flipper').css({'transform' : 'rotate(-15deg)'}).delay(100);
-        $('.flipper').css({'transform' : 'rotate(0)', 'transform' : 'scale(1.03)'}).delay(100);
-      } */
 });
