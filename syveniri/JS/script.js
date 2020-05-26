9f192016cf443bc0c50c474f956085a2be729caf
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-26 14:20:17 +0300

    lego

--- syveniri/JS/script.js
+++ syveniri/JS/script.js
@@ -17,35 +17,46 @@ jQuery(document).ready(function($){
 
         });
       };
+
+      function goto(){
+        location.href = "file:///D:/Github/jazkyDEV.github.io/taxi_2/index.html";
+      };
+
     
     $('.disabled').on('click', function(){
       if($('.current-input').html() == 'A1'){
         money-=20;
         sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+        setTimeout(goto, 21000);
+
       }
     });
     $('.disabled').on('click', function(){
       if($('.current-input').html() == 'A2'){
         money-=32;
         sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+        setTimeout(goto, 21000);
       }
   });
   $('.disabled').on('click', function(){
     if($('.current-input').html() == 'A3'){
       money-=12;
       sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+      setTimeout(goto, 21000);
     }
   });
   $('.disabled').on('click', function(){
     if($('.current-input').html() == 'A4'){
       money-=8;
       sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+      setTimeout(goto, 21000);
     }
   });
   $('.disabled').on('click', function(){
     if($('.current-input').html() == 'A5'){
       money-=5;
       sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+      setTimeout(goto, 21000);
     }
   });
 
@@ -55,30 +66,35 @@ jQuery(document).ready(function($){
     if($('.current-input').html() == 'B1'){
       money-=33;
       sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+      setTimeout(goto, 21000);
     }
   });
   $('.disabled').on('click', function(){
     if($('.current-input').html() == 'B2'){
       money-=28;
       sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+      setTimeout(goto, 21000);
     }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'B3'){
     money-=8;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'B4'){
     money-=9;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'B5'){
     money-=2;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 
@@ -88,30 +104,35 @@ $('.disabled').on('click', function(){
   if($('.current-input').html() == 'C1'){
     money-=19;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'C2'){
     money-=25;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'C3'){
   money-=15;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'C4'){
   money-=6;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'C5'){
   money-=3;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 
@@ -120,30 +141,35 @@ $('.disabled').on('click', function(){
   if($('.current-input').html() == 'D1'){
     money-=25;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'D2'){
     money-=17;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'D3'){
   money-=18;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'D4'){
   money-=9;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'D5'){
   money-=1;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 
@@ -152,30 +178,35 @@ $('.disabled').on('click', function(){
   if($('.current-input').html() == 'E1'){
     money-=21;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
   if($('.current-input').html() == 'E2'){
     money-=22;
     sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+    setTimeout(goto, 21000);
   }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'E3'){
   money-=16;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'E4'){
   money-=11;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 $('.disabled').on('click', function(){
 if($('.current-input').html() == 'E5'){
   money-=4;
   sessionStorage.setItem('counter', JSON.stringify(money.toFixed(1)));
+  setTimeout(goto, 21000);
 }
 });
 
---
+++ taxi_2/JS/script.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($){
+
+    var money = JSON.parse(sessionStorage.getItem('counter'));
+    var hotel = JSON.parse(sessionStorage.getItem('hotel'));
+    $('.counter').html(money);
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 500
+    });
+
+
+    if(money<=0){
+        $(function(){
+          var height = $(document).height();
+          $('#wdw').innerHeight(height);
+          $('.page').css("filter", "blur(10px)");
+          $('#wdw').fadeIn();
+        });
+      };
+
+
+    $('#yandex1').on('click', function(){
+
+    money-=(0.2*hotel);
+    sessionStorage.setItem('counter', JSON.stringify(money));
+
+    });
+
+
+    $('#almaz1').on('click', function(){
+        money-=(0.4*hotel);
+
+        sessionStorage.setItem('counter', JSON.stringify(money));
+
+    });
+
+
+    $('#bavaria1').on('click', function(){
+
+        money-=(0.5*hotel);
+        sessionStorage.setItem('counter', JSON.stringify(money));
+    });
+    
+    
+
+
+});
