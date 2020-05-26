1d572201706b9dc591657571a266aaf9b00cafd1
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-05-26 16:31:50 +0300

    ggs

---
+++ aeroport/JS/script.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function($){
+
+    var money = JSON.parse(sessionStorage.getItem('counter'));
+    money-=150;
+    $('.counter').html(money);
+
+    
+
+
+
+      $('.button').on('click', function(){
+        if(money<=0){
+          $(function(){
+            var height = $(document).height();
+            $('#wdw').innerHeight(height);
+            $('.page').css("filter", "blur(10px)");
+            $('#wdw').fadeIn();
+            $('.counter__item').fadeIn();
+            $('.counter').counterUp({
+              delay: 10,
+              time: 500
+            });
+          });
+        };
+        if(money>=430){
+          $(function(){
+            var height = $(document).height();
+            $('#wdw2').innerHeight(height);
+            $('.page').css("filter", "blur(10px)");
+            $('#wdw2').fadeIn();
+            $('.counter__item').fadeIn();
+            $('.counter').counterUp({
+              delay: 10,
+              time: 500
+            });
+          });
+        };
+        if(money<430 && money>0){
+          $(function(){
+            var height = $(document).height();
+            $('#wdw3').innerHeight(height);
+            $('.page').css("filter", "blur(10px)");
+            $('#wdw3').fadeIn();
+            $('.counter__item').fadeIn();
+            $('.counter').counterUp({
+              delay: 10,
+              time: 500
+            });
+          });
+        };
+      });
+    
+    
+
+
+
+});
