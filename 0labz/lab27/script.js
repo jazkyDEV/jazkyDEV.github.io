88b5dbace65133107f8280107d4c0036f14c8718
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-12-02 19:00:25 +0300

    lab27

---
+++ 0labz/lab27/script.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('img')[0].onmouseover = function(){
+    document.getElementsByTagName('img')[0].src = "gif1s.gif";
+}
+document.getElementsByTagName('img')[0].onmouseout = function(){
+    document.getElementsByTagName('img')[0].src = "gif1.gif";
+}
