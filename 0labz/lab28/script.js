d1cc4200886eb4a01e73f66df39561c343025a21
jazkyDEV <60138523+jazkyDEV@users.noreply.github.com>
2020-12-16 00:01:09 +0300

    lab28

--- 0labz/lab28/script.js
+++ 0labz/lab28/script.js
@@ -1,9 +1,16 @@
-document.getElementById('contacts').onmouseover = function() {
-    var ele = document.querySelector('#contacts .header__nav-link-menu');
-    ele.style.display = "block";
+var i = 0;
+
+document.getElementsByClassName("section__menu-button")[0].onclick = function(){
+var ele = document.getElementsByClassName("section__menu")[0];
+var menu = document.getElementsByClassName("menu__icon")[0];
+if(i % 2 == 0){
+    ele.style.transform = "translate(0, -50%)";
+    menu.style.transform = "rotate(-90deg)";
+}
+else {
+    ele.style.removeProperty("transform");
+    menu.style.removeProperty("transform");
 
 }
-document.getElementById('contacts').onmouseout = function() {
-    var ele = document.querySelector('#contacts .header__nav-link-menu');
-    ele.style.display = "none";
+++i;
 }
